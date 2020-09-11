5d01037b8423e7bb5df9af1bb94dba2937c2e769
Vitalii <vit.bondarchuk@gmail.com>
2020-09-11 22:22:21 +0300

    Updates

---
+++ precache-manifest.b0d63cf48119fd7a9ee95103f69d326a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d09b1407e3d6de1980c39e876e3620",
+    "url": "/hw17_react_hooks/index.html"
+  },
+  {
+    "revision": "848de8eb100217d15303",
+    "url": "/hw17_react_hooks/static/css/main.e23b3035.chunk.css"
+  },
+  {
+    "revision": "32c50c9a725216a5e421",
+    "url": "/hw17_react_hooks/static/js/2.5b3596ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw17_react_hooks/static/js/2.5b3596ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848de8eb100217d15303",
+    "url": "/hw17_react_hooks/static/js/main.1e03ca80.chunk.js"
+  },
+  {
+    "revision": "ec324459f4c10c5692f7",
+    "url": "/hw17_react_hooks/static/js/runtime-main.7e71d53e.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/hw17_react_hooks/static/media/digital_mono.58045dab.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw17_react_hooks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f23c27e54d2db50a7bc2e864d9a4150b",
+    "url": "/hw17_react_hooks/static/media/teddy_alert.f23c27e5.mp3"
+  }
+]);
